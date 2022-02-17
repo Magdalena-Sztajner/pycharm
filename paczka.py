5c8bd7e4bb4c0b6b913a76e79667efc59452ee7d
Magdalena <sztajner.m123@gmail.com>
2022-02-17 15:49:37 +0100

    warunek if do zliczenia max pustych kg nr paczki z max pustych kg

--- paczka.py
+++ paczka.py
@@ -41,9 +41,13 @@ while waga_elementu and waga_elementu <= 10:
     # wysłanie ostatniego elementu/elementów, (jeżeli nie było błędu wszystkie elementy muszą zostać wysłane)
     # dodane 404 by pozbyć się błędu "EOFError: EOF when reading a line"
     if waga_elementu == 404 and waga_ostatniego_elementu > 0:
-    print("Koniec elementów! Wysyłam resztę.")
-    waga_paczki = waga_paczki - waga_elementu
-    wyslane_paczki = wyslane_paczki + 1
-    puste_kg = 20 - waga_ostatniego_elementu
-    suma_pustych_kg += puste_kg
-    razem_kg += waga_ostatniego_elementu
+        print("Koniec elementów! Wysyłam resztę.")
+        waga_paczki = waga_paczki - waga_elementu
+        wyslane_paczki = wyslane_paczki + 1
+        puste_kg = 20 - waga_ostatniego_elementu
+        suma_pustych_kg += puste_kg
+        razem_kg += waga_ostatniego_elementu
+
+    if puste_kg > max_pustych_kg:
+        max_pustych_kg = puste_kg
+        nr_paczki = wyslane_paczki
