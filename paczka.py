e764d6508a0bd06e766adc52bd574909e5b00491
Magdalena <sztajner.m123@gmail.com>
2022-02-17 15:43:37 +0100

    Dodanie petli while z warunkami if, elif i else

--- paczka.py
+++ paczka.py
@@ -8,3 +8,30 @@ razem_kg = 0
 max_pustych_kg = 0
 nr_paczki = 0
 
+while waga_elementu and waga_elementu <= 10:
+
+    waga_paczki += waga_elementu
+
+    if waga_paczki < 20:
+        print("Dobieram element")
+        max_pustych_kg = puste_kg
+        waga_ostatniego_elementu = waga_paczki
+
+    elif waga_paczki == 20:
+        print("Idealnie! Wysyłam")
+        razem_kg += waga_paczki
+        puste_kg = 20 - waga_paczki
+        suma_pustych_kg += puste_kg
+        wyslane_paczki = wyslane_paczki + 1
+        waga_paczki = 0
+        waga_ostatniego_elementu = 0
+
+    else:
+        print("Maksymalna waga paczki przekroczona! Odejmuję element i wysyłam")
+        waga_paczki -= waga_elementu
+        wyslane_paczki = wyslane_paczki + 1
+        puste_kg = 20 - waga_paczki
+        suma_pustych_kg += puste_kg
+        razem_kg += waga_paczki
+        waga_paczki = waga_elementu
+        waga_ostatniego_elementu = waga_elementu
