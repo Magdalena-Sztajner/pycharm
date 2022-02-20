8ebefdc4acdc45d89c92ca0e0bdea86078fa584b
Magdalena <sztajner.m123@gmail.com>
2022-02-20 17:38:37 +0100

    Stworzenie petli while z if i else

--- Collatz.py
+++ Collatz.py
@@ -3,3 +3,12 @@ wybrana_liczba = int(input())
 dlugosc_ciagu = 0
 liczba_poczatkowa = wybrana_liczba
 
+while wybrana_liczba != 1:
+
+      if wybrana_liczba % 2 == 0:
+            wybrana_liczba = wybrana_liczba/2
+            dlugosc_ciagu += 1
+
+      else:
+            wybrana_liczba = 3 * wybrana_liczba + 1
+            dlugosc_ciagu += 1
