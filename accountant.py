68cf5a09943e0c54a1f5709b126c42490e0f2d26
Magdalena <sztajner.m123@gmail.com>
2022-03-02 20:07:15 +0100

    Utworzenie zmiennych, rozpoczecie petli (wykluczone akcje i saldo)

---
+++ accountant.py
@@ -0,0 +1,29 @@
+akcja = input()
+historia = []
+hist_tmp = []
+saldo = 0
+magazyn = {}
+ilosc_szt = 0
+
+while True:
+
+
+    if akcja not in ["saldo", "zakup", "sprzedaz"]:
+        print()
+        print("Brak akcji saldo/sprzedaz/zakup!")
+        print("Nieprawidlowy input!")
+        print()
+        exit()
+
+    if akcja == "saldo":
+        saldo_zmiana = int(input())
+        saldo_koment = input()
+
+        hist_tmp.append(akcja)
+        hist_tmp.append(saldo_zmiana)
+        hist_tmp.append(saldo_koment)
+
+        saldo += (saldo_zmiana)
+
+        historia.append(hist_tmp)
+        hist_tmp = []
