5ff6e40e2ea4a0cf638fc4842456482bbd206901
Magdalena <sztajner.m123@gmail.com>
2022-03-02 20:10:48 +0100

    Petla (akcja sprzedaz)

--- accountant.py
+++ accountant.py
@@ -56,3 +56,57 @@ while True:
             ilosc_szt = x + szt_zakup
             magazyn[produkt_zakup] = ilosc_szt
 
+
+    elif akcja == "sprzedaz":
+        produkt_sprzedaz = input()
+        cena_sprzedazy = int(input())
+        szt_sprzedaz = int(input())
+
+
+        hist_tmp.append(akcja)
+        hist_tmp.append(produkt_sprzedaz)
+        hist_tmp.append(cena_sprzedazy)
+        hist_tmp.append(szt_sprzedaz)
+
+        saldo = saldo + (cena_sprzedazy * szt_sprzedaz)
+
+        historia.append(hist_tmp)
+        hist_tmp = []
+
+        if produkt_sprzedaz not in magazyn:
+            print()
+            print("Błąd!""\nNie można sprzedać produktu którego nie ma w magazynie!")
+            print()
+            exit()
+        if produkt_sprzedaz in magazyn:
+            x = magazyn[produkt_sprzedaz]
+            ilosc_szt = x - szt_sprzedaz
+            magazyn[produkt_sprzedaz] = ilosc_szt
+
+            if ilosc_szt == 0:
+                del magazyn[produkt_sprzedaz]
+
+            elif ilosc_szt <0:
+                print()
+                print("Nieprawidłowa operacja: za duża ilość sprzedanych elementów!")
+                print()
+                exit()
+
+    akcja = input()
+    if akcja == "stop":
+        break
+
+
+# print()
+#
+# for element in historia:
+#     print(element)
+#
+# print()
+#
+# print("Saldo:", saldo)
+#
+# print()
+#
+# print("Magazyn:", magazyn)
+
