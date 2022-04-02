68a7271fca92dd4fb3aff4f2c0cf0fd2cdc5dbd6
Magdalena <sztajner.m123@gmail.com>
2022-04-02 15:41:04 +0200

    Sprawdzanie historii, If i else

--- weather.py
+++ weather.py
@@ -19,3 +19,22 @@ except:
     data_argv = str(date + timedelta(days=1))
     # print(date + timedelta(days=1))
 
+wwa_lat = 52.2319581
+wwa_lon = 21.0067249
+API_url = F"https://api.openweathermap.org/data/2.5/onecall?lat={wwa_lat}&lon={wwa_lon}&exclude=hourly,minutely,alerts&appid={API_ARGV}"
+
+pogoda = {}
+
+if os.stat("historia_pogody.json").st_size > 0:
+    print("Sprawdzam historię...")
+    with open("historia_pogody.json", "r") as plik:
+        sprawdzenie_historii = json.load(plik)
+        wyszukiwanie = sprawdzenie_historii.get(data_argv)
+        if wyszukiwanie:
+            print(F"{data_argv}: {wyszukiwanie}")
+            exit()
+        else:
+            print("Wysyłam zapytanie...")
+
+else:
+    print("Wysyłam zapytanie...")
