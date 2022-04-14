04c5d87db952195171141f176c83641670f5062e
Magdalena <sztajner.m123@gmail.com>
2022-04-14 11:24:13 +0200

    def historia_zapisu

--- weather2.0/weather2.0.py
+++ weather2.0/weather2.0.py
@@ -24,3 +24,18 @@ class WeatherForecast():
         API_URL = F"https://api.openweathermap.org/data/2.5/onecall?lat={wwa_lat}&lon={wwa_lon}&exclude=hourly,minutely,alerts&appid={klucz_API}"
         odpowiedz = requests.get(API_URL)
         self.zapytanie = odpowiedz.json()
+
+    def historia_zapis(self):
+        pogoda_ = wf.pogoda
+
+        try:
+            with open("historia_pogody.json", "r") as plik:
+                aktualizacja_historii_pogody = json.load(plik)
+                aktualizacja_historii_pogody.update(pogoda_)
+
+            with open("historia_pogody.json", "w") as plik:
+                json.dump(aktualizacja_historii_pogody, plik, sort_keys=True, indent=4, separators=(',', ': '))
+
+        except:
+            with open("historia_pogody.json", "w") as plik:
+                json.dump(pogoda_, plik, sort_keys=True, indent=4, separators=(',', ': '))
