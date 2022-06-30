8ed5fa13559210401606b0628fe954ea6c6e12d3
Magdalena <sztajner.m123@gmail.com>
2022-06-30 18:22:04 +0200

    Petla for

--- zadanie1_ver2.py
+++ zadanie1_ver2.py
@@ -4,3 +4,11 @@ pozyczka = 12000
 mniej_0 = 0
 splacone = 0
 
+for i in range(12):
+    miesiac = input()
+    inflacja = float(input())
+
+    do_splaty_lacznie = round((1 + ((inflacja + oprocentowanie) / 1200)) * pozyczka - rata)
+    mniej_0 = round(pozyczka - do_splaty_lacznie)
+    # pozyczka -= mniej_0
+    pozyczka = do_splaty_lacznie
