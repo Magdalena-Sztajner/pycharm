7f722dd8053df21028d1d828d087462f2bf83c64
Magdalena <sztajner.m123@gmail.com>
2022-08-02 17:23:48 +0200

    Zmienne

---
+++ mechanics.py
@@ -0,0 +1,14 @@
+import math
+#
+m1_godziny = 0
+m2_godziny = 0
+m3_godziny = 0
+lacznie_czas_naprawy = 0
+m1_dni_pracy = 0
+m2_dni_pracy = 0
+m3_dni_pracy = 0
+dyspozycyjnosc_wszystkich_mechanikow = 0
+max_liczba_dni = 0
+numer_kolejnych_zlecenia = 0
+numer_zlecenia = 1
+lista_dyspo = []
