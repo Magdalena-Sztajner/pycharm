0446cd9d10762d948eac5b4e94447a21589e4ae9
Magdalena <sztajner.m123@gmail.com>
2022-08-02 17:26:26 +0200

    Warunki: if, elif i else

--- mechanics.py
+++ mechanics.py
@@ -18,3 +18,29 @@ numer = liczba_zlecen
 for i in range(numer):
         numer_kolejnych_zlecenia += numer_zlecenia
         czas_naprawy = int(input(f"Ile czasu zajmie naprawa nr {numer_kolejnych_zlecenia}? : "))
+        if m1_godziny <= m2_godziny and m1_godziny <= m3_godziny:
+            m1_godziny += czas_naprawy
+            m1_dni_pracy = math.ceil(m1_godziny / 8)
+            lista_dyspo.append(m1_dni_pracy)
+
+
+        elif m2_godziny > m3_godziny:
+            m3_godziny += czas_naprawy
+            m3_dni_pracy = math.ceil(m3_godziny / 8)
+            lista_dyspo.append(m3_dni_pracy)
+
+        else:
+            m2_godziny += czas_naprawy
+            m2_dni_pracy = math.ceil(m2_godziny / 8)
+            lista_dyspo.append(m2_dni_pracy)
+
+        if m1_dni_pracy > m2_dni_pracy and m1_dni_pracy > m3_dni_pracy:
+            max_liczba_dni = m1_dni_pracy
+
+        elif m2_dni_pracy > m3_dni_pracy:
+            max_liczba_dni = m2_dni_pracy
+
+        else:
+            max_liczba_dni = m3_dni_pracy
+
+
